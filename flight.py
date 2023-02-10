a783b509b6fea7ef70fa61560dbec8f95c9e53da
LeeSeungjae00 <seungjae266789@gmail.com>
2023-02-10 16:39:09 +0900

    :tada: Begin a project
    크롤링 기본 기능 추가

---
+++ flight.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+import requests
+import json
+
+# 웹 드라이버 초기화
+def set_chrome_driver():
+    chrome_options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    return driver
+
+
+driver = set_chrome_driver()
+driver.implicitly_wait(5)
+#
+def get_min_price():
+    videos, titles = [], []
+    row = 1
+    col = 1
+    start_url = "https://m-flight.naver.com/flights/international/ICN-KIX-20230415/KIX-ICN-20230419?adult=2&fareType=Y"
+    driver.get(start_url)
+
+    while(1):
+        try:
+            path = "/html/body/div[1]/div/div[1]/div[6]/div/div[3]/div[1]/div/div[2]/div/div/div/div/b/i"
+            min_price = driver.find_element_by_xpath(path).get_attribute('innerText')
+            return min_price
+        except Exception as e:
+            a = 1
+
+    return '123'
+# 비어있는 text
+
+while(1):
+    price = get_min_price()
+    int_price = price.replace(',', "")
+    int_price = int(int_price)
+
+    if int_price < 360000:
+        url = "https://hooks.slack.com/services/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"
+        # response = requests.get(url_items)
+
+        headers = {'Content-Type': 'application/json', 'charset': 'UTF-8', 'Accept': '*/*'}
+        body = {
+            "text": "https://m-flight.naver.com/flights/international/ICN-KIX-20230415/KIX-ICN-20230419?adult=2&fareType=Y gogogo!!!"
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(body, ensure_ascii=False, indent="\t"))
+        print(response)
+        print("떳다")
+    print(int_price)
+
